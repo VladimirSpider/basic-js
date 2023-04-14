08184a364b14f845207ebebf640e0e11dc31d451
Vladimir <vladimir.bur@overdrive.sg>
2023-04-14 18:40:57 +0300

    fix: created scratch of function getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (typeof date !== 'object') {
+    return 'Invalid date!';
+  }
+  const month = date.getMonth();
+  if (month >= 2 && month <= 4) {
+    console.log(month);
+    return 'spring';
+  }
+  if (month >= 5 && month <= 7) {
+    console.log(month);
+    return 'summer';
+  }
+  if (month >= 8 && month <= 10) {
+    console.log(month);
+    return 'autumn';
+  }
+  if (month === 0 || month === 1 || month === 11) {
+    console.log(month);
+    return 'winter';
+  }
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
