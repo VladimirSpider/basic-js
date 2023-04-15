5faac11ec5cdba63a0ec4fd11047639b29cbd809
Vladimir <vladimir.bur@overdrive.sg>
2023-04-15 17:28:20 +0300

    fix: created function getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -13,25 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(date) {
   if (!date) return 'Unable to determine the time of year!';
-  if (typeof date !== 'object') {
-    return 'Invalid date!';
-  }
-  const month = date.getMonth();
-  if (month >= 2 && month <= 4) {
-    console.log(month);
-    return 'spring';
-  }
-  if (month >= 5 && month <= 7) {
-    console.log(month);
-    return 'summer';
-  }
-  if (month >= 8 && month <= 10) {
-    console.log(month);
-    return 'autumn';
-  }
-  if (month === 0 || month === 1 || month === 11) {
-    console.log(month);
-    return 'winter';
+  try {
+    const arr = Object.entries(date);
+    if (arr.length) {
+      throw new Error("Invalid date!");
+    }
+    const month = date.getMonth();
+    if (month >= 2 && month <= 4) {
+      console.log(month);
+      return 'spring';
+    }
+    if (month >= 5 && month <= 7) {
+      console.log(month);
+      return 'summer';
+    }
+    if (month >= 8 && month <= 10) {
+      console.log(month);
+      return 'autumn';
+    }
+    if (month === 0 || month === 1 || month === 11) {
+      console.log(month);
+      return 'winter';
+    }
+  } catch (error) {
+    throw new Error("Invalid date!");
   }
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
