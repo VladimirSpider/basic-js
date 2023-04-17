0c73b0d67c7ebcc649fe8c8620c3eaa2cc738df3
Vladimir <vladimir.bur@overdrive.sg>
2023-04-17 13:47:02 +0300

    fix: created scratch function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity === "string" && Number(sampleActivity)) {
+    return 1;
+  } else {
+    return false;
+  }
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
