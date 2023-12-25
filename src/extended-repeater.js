050cf9a85e98d69d3972db69b71955a8eb075057
vladimir <vovchikbura@mail.ru>
2023-12-25 14:45:51 +0300

    feat: created repeater Function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let separator = 'separator' in options ? options['separator'] : '+';
+  let additionSeparator  = 'additionSeparator' in options ? options['additionSeparator'] : '|';
+  let repeatTimes = 'repeatTimes' in options ? options['repeatTimes'] : 1;
+  let additionRepeatTimes  = 'additionRepeatTimes' in options ? options['additionRepeatTimes'] : 1;
+  let addition = 'addition' in options ? String(options['addition']) : '';
+
+  const additionFullSeparator = Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+  const fullNewString = Array(repeatTimes).fill(str + additionFullSeparator).join(separator);
+
+  return fullNewString;
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
